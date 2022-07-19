e7d3759c20532f482aff2a410cd51898d4773c1f
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-07-19 04:06:42 +0100

    make use of stack and put the results in the example run

--- Engine/handler.py
+++ Engine/handler.py
@@ -8,6 +8,6 @@ def choose_framework(framework: str):
     global framework_stack
     backend = importlib.import_module("Engine.frameworks." + framework)
     framework_stack.append(backend)
-    for key, value in backend.__dict__.items():
+    for key, value in framework_stack[-1].__dict__.items():
         Engine.__dict__[key] = value
 
--- example_run.py
+++ example_run.py
@@ -10,3 +10,20 @@ for fw in frameworks_available:
     print(x, type(x))
     print(y, type(y))
     print()
+
+'''
+tensorflow
+tf.Tensor(
+[[1 2 3]
+ [4 5 6]], shape=(2, 3), dtype=int32) <class 'tensorflow.python.framework.ops.EagerTensor'>
+tf.Tensor([ 6 15], shape=(2,), dtype=int32) <class 'tensorflow.python.framework.ops.EagerTensor'>
+
+numpy
+[[1 2 3]
+ [4 5 6]] <class 'numpy.ndarray'>
+[ 6 15] <class 'numpy.ndarray'>
+
+torch
+tensor([[1, 2, 3],
+        [4, 5, 6]]) <class 'torch.Tensor'>
+tensor([ 6, 15]) <class 'torch.Tensor'>'''

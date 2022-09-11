c2dc6a7913c88ec5b9a21f8ef76011713149fff1
Ishtiaq Hussain <53497039+Ishticode@users.noreply.github.com>
2022-09-11 11:42:44 +0100

    adapting conv2d from ivy 1.1.9

--- Engine/frameworks/tensorflow/layer_functions.py
+++ Engine/frameworks/tensorflow/layer_functions.py
@@ -5,17 +5,18 @@ def linear(x, weight, bias=None):
     return (tf.matmul(x, tf.transpose(weight)) + bias) if bias is not None else tf.matmul(x, tf.transpose(weight))
 
 
-def conv2d(x, filters, strides, padding, data_format='NHWC'):
+def conv2d(x, filters, strides, padding, data_format='NHWC', dilations=1):
     if data_format == 'NCHW':
         x = tf.transpose(x, (0, 2, 3, 1))
-    res = tf.nn.conv2d(x, filters, strides, padding, 'NHWC')
+    res = tf.nn.conv2d(x, filters, strides, padding, 'NHWC', dilations)
     if data_format == 'NCHW':
-        return tf.transpose(res, (0, 3, 1, 2))
+        return _tf.transpose(res, (0, 3, 1, 2))
     return res
 
 
 def relu(x):
     return tf.nn.relu(x)
+
 
 def max_pool(x, ksize, strides, data_format='NHWC'):
     if data_format == 'NCHW':
@@ -25,9 +26,6 @@ def max_pool(x, ksize, strides, data_format='NHWC'):
         return tf.transpose(res, (0, 3, 1, 2))
     return res
 
-
-def cross_entropy_loss(x, y):
-    return tf.nn.softmax_cross_entropy_with_logits(x, y)
 
 def mse_loss(x, y):
     return tf.reduce_sum(tf.losses.mean_squared_error(x, y))
--- Engine/frameworks/torch/layer_functions.py
+++ Engine/frameworks/torch/layer_functions.py
@@ -1,12 +1,12 @@
 import torch
 import math
 
+
 def linear(x, weight, bias=None):
     return torch.nn.functional.linear(x, weight, bias)
 
 
-# noinspection PyUnresolvedReferences
-def conv2d(x, filters, strides: int, padding: str, data_format: str = 'NHWC'):
+def conv2d(x, filters, strides: int, padding: str, data_format: str = 'NHWC', dilations: int = 1):
     filter_shape = list(filters.shape[0:2])
     filters = filters.permute(3, 2, 0, 1)
     if data_format == 'NHWC':
@@ -18,7 +18,7 @@ def conv2d(x, filters, strides: int, padding: str, data_format: str = 'NHWC'):
     else:
         raise Exception('Invalid padding arg {}\n'
                         'Must be one of: "VALID" or "SAME"'.format(padding))
-    res = torch.nn.functional.conv2d(x, filters, None, strides, padding_list)
+    res = torch.nn.functional.conv2d(x, filters, None, strides, padding_list, dilations)
     if data_format == 'NHWC':
         return res.permute(0, 2, 3, 1)
     return res
@@ -33,9 +33,6 @@ def max_pool(x, ksize, strides):
     res = torch.nn.functional.max_pool2d(x, ksize, strides)
     return res.permute(0, 3, 1, 2)
 
-
-def cross_entropy_loss(x, y):
-    return (torch.nn.functional.cross_entropy(x, y))
 
 def mse_loss(x, y):
     return torch.nn.functional.mse_loss(x, y)
